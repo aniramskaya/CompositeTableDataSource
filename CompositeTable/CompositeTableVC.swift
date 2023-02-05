e0b1421a0ddae4dab6d5bd8adc43e966787a65ca
Marina Chemezova <aniramskaya@gmail.com>
2023-02-05 18:49:16 +0500

    Rename ViewController to CompositeTableVC

--- CompositeTable/CompositeTableVC.swift
+++ CompositeTable/CompositeTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CompositeTableVC.swift
 //  CompositeTable
 //
 //  Created by Марина Чемезова on 05.02.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CompositeTableVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
